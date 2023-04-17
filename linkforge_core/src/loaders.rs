6f824018eebdb4a94ce6c5f0ba55b660b58df226
Makris Spiros <makris.spiros@gmail.com>
2023-04-17 12:42:03 +0300

    add a simple text file loader

--- linkforge_core/src/loaders.rs
+++ linkforge_core/src/loaders.rs
@@ -1,3 +1,5 @@
 mod document_loader;
+mod text_file_loader;
 
 pub use document_loader::*;
+pub use text_file_loader::*;
---
+++ linkforge_core/src/loaders/text_file_loader.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+use crate::{Document, DocumentLoader, LoaderError};
+
+pub struct TextFileLoader {
+    pub path: String,
+}
+
+impl TextFileLoader {
+    pub fn new(path: String) -> Self {
+        Self { path }
+    }
+}
+
+/// This is a DocumentLoader implementation for simple text
+/// files. Metadata added is the file source path and the
+/// filename.
+/// Currently supports UTF-8 encoded text files only.
+/// Invalid UTF-8 characters in source file error out.
+impl DocumentLoader for TextFileLoader {
+    fn load(&self) -> Result<Vec<Document>, LoaderError> {
+        let content = read_text_file(&self.path)?;
+        let mut metadata = HashMap::new();
+        metadata.insert("source_file".to_string(), self.path.clone());
+
+        let doc = Document {
+            content: content.into(),
+            metadata,
+        };
+
+        Ok(vec![doc])
+    }
+}
+
+fn read_text_file<P: AsRef<Path>>(path: P) -> Result<String, LoaderError> {
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)?;
+
+    Ok(content)
+}
