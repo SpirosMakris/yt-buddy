1b6f5ce04632f3431c588d1f535f0e5b05f557d2
Makris Spiros <makris.spiros@gmail.com>
2023-04-25 10:40:00 +0300

    remove unused

--- yt_kb_core/src/lib.rs
+++ yt_kb_core/src/lib.rs
@@ -5,12 +5,4 @@ pub use loaders::*;
 pub use schema::*;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
