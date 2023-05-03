2d2d467fd60ba1f026c4fe480e8a7167048874b2
Makris Spiros <makris.spiros@gmail.com>
2023-05-03 13:25:46 +0300

    remove surrealdb deps

--- yt_buddy/src/bin/db_ingest_example.rs
+++ yt_buddy/src/bin/db_ingest_example.rs
@@ -1,107 +1,110 @@
-use chrono::Utc;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
-use surrealdb::sql::Thing;
-use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, Surreal};
+// @REMOVE
+// use chrono::Utc;
+// use serde::{Deserialize, Serialize};
+// use serde_json::json;
+// use surrealdb::sql::Thing;
+// use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, Surreal};
 
-use yt_buddy_core::{DocumentLoader, YoutubeCaptionsLoader};
+// use yt_buddy_core::{DocumentLoader, YoutubeCaptionsLoader};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-enum IngestStatus {
-    Pending,
-    Loading,
-    Loaded,
-    Embedding,
-    Done,
-}
+fn main() {}
 
-#[derive(Debug, Serialize, Deserialize)]
-struct IngestYTVideoItem {
-    #[serde(skip_serializing)]
-    #[allow(dead_code)]
-    id: Option<Thing>,
-    video_id: String,
-    status: IngestStatus,
-    text: Option<String>,
-    created_at: chrono::DateTime<Utc>,
-    updated_at: chrono::DateTime<Utc>,
-}
-
-// #[derive(Debug, Deserialize)]
-// struct IngestYTVideoRecord {
-//     id: Thing,
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(rename_all = "snake_case")]
+// enum IngestStatus {
+//     Pending,
+//     Loading,
+//     Loaded,
+//     Embedding,
+//     Done,
 // }
 
-#[tokio::main]
-async fn main() -> surrealdb::Result<()> {
-    println!("Connecting to surrealdb..");
+// #[derive(Debug, Serialize, Deserialize)]
+// struct IngestYTVideoItem {
+//     #[serde(skip_serializing)]
+//     #[allow(dead_code)]
+//     id: Option<Thing>,
+//     video_id: String,
+//     status: IngestStatus,
+//     text: Option<String>,
+//     created_at: chrono::DateTime<Utc>,
+//     updated_at: chrono::DateTime<Utc>,
+// }
 
-    // Connect to server
-    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
-    // println!("{db:?}");
+// // #[derive(Debug, Deserialize)]
+// // struct IngestYTVideoRecord {
+// //     id: Thing,
+// // }
 
-    // Sign-in as a namespace, db or root user
-    db.signin(Root {
-        username: "root",
-        password: "root",
-    })
-    .await?;
+// #[tokio::main]
+// async fn main() -> surrealdb::Result<()> {
+//     println!("Connecting to surrealdb..");
 
-    // Select a specific namespace/db
-    db.use_ns("test").use_db("yt_buddy_db").await?;
+//     // Connect to server
+//     let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+//     // println!("{db:?}");
 
-    let video_id = "IqqHqDcXLww";
+//     // Sign-in as a namespace, db or root user
+//     db.signin(Root {
+//         username: "root",
+//         password: "root",
+//     })
+//     .await?;
 
-    let id = format!("{video_id}|{}", uuid::Uuid::new_v4());
+//     // Select a specific namespace/db
+//     db.use_ns("test").use_db("yt_buddy_db").await?;
 
-    // Create a new ingest queue entry
-    let _created_entry: IngestYTVideoItem = db
-        .create(("ingest_queue", id.clone()))
-        .content(IngestYTVideoItem {
-            id: None,
-            video_id: video_id.to_string(),
-            text: None,
-            status: IngestStatus::Pending,
-            created_at: chrono::Utc::now(),
-            updated_at: chrono::Utc::now(),
-        })
-        .await?;
+//     let video_id = "IqqHqDcXLww";
 
-    // dbg!(created_entry);
+//     let id = format!("{video_id}|{}", uuid::Uuid::new_v4());
 
-    // Select all ingest records
-    // let ingest_items: Vec<IngestYTVideoItem> = db.select("ingest_queue").await?;
+//     // Create a new ingest queue entry
+//     let _created_entry: IngestYTVideoItem = db
+//         .create(("ingest_queue", id.clone()))
+//         .content(IngestYTVideoItem {
+//             id: None,
+//             video_id: video_id.to_string(),
+//             text: None,
+//             status: IngestStatus::Pending,
+//             created_at: chrono::Utc::now(),
+//             updated_at: chrono::Utc::now(),
+//         })
+//         .await?;
 
-    // dbg!(ingest_items);
+//     // dbg!(created_entry);
 
-    // Select oldest entry
-    let oldest_queue_entry: Option<IngestYTVideoItem> = db
-        .query("SELECT * FROM type::table($table) WHERE status='pending' ORDER BY created_at ASC LIMIT 1;")
-        .bind(("table", "ingest_queue"))        
-        .await?
-        .take(0)?;
+//     // Select all ingest records
+//     // let ingest_items: Vec<IngestYTVideoItem> = db.select("ingest_queue").await?;
 
-    dbg!(&oldest_queue_entry);
+//     // dbg!(ingest_items);
 
-    if let Some(item) = oldest_queue_entry {
-        let loader = YoutubeCaptionsLoader::new(item.video_id.clone());
-        let res = loader.load().await.unwrap();
+//     // Select oldest entry
+//     let oldest_queue_entry: Option<IngestYTVideoItem> = db
+//         .query("SELECT * FROM type::table($table) WHERE status='pending' ORDER BY created_at ASC LIMIT 1;")
+//         .bind(("table", "ingest_queue"))
+//         .await?
+//         .take(0)?;
 
-        dbg!(&res);
-        dbg!(item);
+//     dbg!(&oldest_queue_entry);
 
-        // debug_assert!(&res.len() == 1usize);
+//     if let Some(item) = oldest_queue_entry {
+//         let loader = YoutubeCaptionsLoader::new(item.video_id.clone());
+//         let res = loader.load().await.unwrap();
 
-        let text = res.get(0).map(|x| x.page_content.clone());
+//         dbg!(&res);
+//         dbg!(item);
 
-        let update_res: IngestYTVideoItem = db
-            .update(("ingest_queue", id.clone()))
-            .merge(json!({ "status": IngestStatus::Done, "text": text }))
-            .await?;
+//         // debug_assert!(&res.len() == 1usize);
 
-        dbg!(update_res);
-    }
+//         let text = res.get(0).map(|x| x.page_content.clone());
 
-    Ok(())
-}
+//         let update_res: IngestYTVideoItem = db
+//             .update(("ingest_queue", id.clone()))
+//             .merge(json!({ "status": IngestStatus::Done, "text": text }))
+//             .await?;
+
+//         dbg!(update_res);
+//     }
+
+//     Ok(())
+// }
