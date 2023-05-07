9c673ab276f68ba5ed8a47ddb71403a7010de6f9
Makris Spiros <makris.spiros@gmail.com>
2023-05-07 11:08:39 +0300

    move loaders to separate crate

--- sm_llm_loaders/src/lib.rs
+++ sm_llm_loaders/src/lib.rs
@@ -1,9 +1,9 @@
-mod document_loader;
 mod pdf_loader;
 mod text_file_loader;
+mod traits;
 mod youtube_captions_loader;
 
-pub use document_loader::*;
 pub use pdf_loader::*;
 pub use text_file_loader::*;
+pub use traits::*;
 pub use youtube_captions_loader::*;
--- yt_buddy_core/src/lib.rs
+++ yt_buddy_core/src/lib.rs
@@ -1,10 +1,8 @@
 mod embeddings;
-mod loaders;
 mod retrievers;
 mod text_splitter;
 
 pub use embeddings::*;
-pub use loaders::*;
 pub use retrievers::*;
 pub use text_splitter::*;
 
