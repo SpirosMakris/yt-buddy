8286453fd472f6f00ca2c9eed26a1103d5798873
Makris Spiros <makris.spiros@gmail.com>
2023-05-11 07:21:58 +0300

    add static routes

--- yt_buddy/src/main.rs
+++ yt_buddy/src/main.rs
@@ -3,28 +3,39 @@ use std::net::SocketAddr;
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handle_hello2));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     // region: axum::Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("->> LISTENING on {}\n", addr);
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
     // endregion: axum::Server
 }
 
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./public")))
+}
+
 // region: Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handle_hello2))
+}
+
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
--- yt_buddy/tests/quick_dev.rs
+++ yt_buddy/tests/quick_dev.rs
@@ -5,5 +5,7 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
     hc.do_get("/hello2/Spiros").await?.print().await?;
+    // hc.do_get("/ingest.html").await?.print().await?;
+
     Ok(())
 }
